d0f42e61d9a8322cd90e69eb5650d6ef03690832
LtWilhelm <ironicallyshort737@hotmail.com>
2021-04-02 13:01:30 -0600

    maybe now?

--- db/dbConfig.ts
+++ db/dbConfig.ts
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongo";
+import "https://deno.land/x/dotenv/load.ts";
 
 const client = new MongoClient();
+console.log(Deno.env.get('MONGODB_URI'));
 await client.connect(Deno.env.get('MONGODB_URI')!);
 
 export default client.database('class-scores');
